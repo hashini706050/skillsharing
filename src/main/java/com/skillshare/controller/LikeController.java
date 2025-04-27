6f5e1a09caac8e519646313e5e68606d0dd2c950
HashiniJ <it22171924@my.sliit.lk>
2025-04-28 05:02:00 +0530

    commit-15-implement-repository

--- src/main/java/com/skillshare/controller/LikeController.java
+++ src/main/java/com/skillshare/controller/LikeController.java
@@ -1,9 +1,26 @@
 package com.skillshare.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillshare.model.Like;
+import com.skillshare.repository.LikeRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @RestController
 @RequestMapping("/posts/{postId}/likes")
 @RequiredArgsConstructor
 public class LikeController {
-    
+    private final LikeRepository likeRepository;
+    private final PostRepository postRepository;
+    private final NotificationRepository notificationRepository;
+
 }
