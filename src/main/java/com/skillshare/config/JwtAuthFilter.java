ac198fda092c557093a76ea414f78bd58b58f81d
Tharaka-Randeniya <dananjanirandeniya1@gmail.com>
2025-04-27 19:56:45 +0530

    commit-7-JwtAuthFilter-config-fix-3

--- src/main/java/com/skillshare/config/JwtAuthFilter.java
+++ src/main/java/com/skillshare/config/JwtAuthFilter.java
@@ -7,5 +7,18 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     public JwtAuthFilter(@Lazy JwtService jwtService) {
         this.jwtService = jwtService;
     }
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
     
 }
