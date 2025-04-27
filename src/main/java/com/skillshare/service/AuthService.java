72832077b7ada3b4a48773fe53a572627dee056b
Tharaka-Randeniya <dananjanirandeniya1@gmail.com>
2025-04-28 00:50:54 +0530

    commit-21-AuthService-service-fix-1

--- src/main/java/com/skillshare/service/AuthService.java
+++ src/main/java/com/skillshare/service/AuthService.java
@@ -1,5 +1,10 @@
 package com.skillshare.service;
 
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    
     
 }
