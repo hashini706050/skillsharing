aff0dd27577e0c9966907412e6c35a75716136db
Tharaka-Randeniya <dananjanirandeniya1@gmail.com>
2025-04-27 19:51:25 +0530

    commit-6-JwtAuthFilter-config-fix-2

--- src/main/java/com/skillshare/config/JwtAuthFilter.java
+++ src/main/java/com/skillshare/config/JwtAuthFilter.java
@@ -3,5 +3,9 @@ package com.skillshare.config;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+
+    public JwtAuthFilter(@Lazy JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
     
 }
