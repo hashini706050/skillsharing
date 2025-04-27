e6a810b6e17d40b3606250128a8b92082c45911d
HashiniJ <it22171924@my.sliit.lk>
2025-04-28 04:55:59 +0530

    commit-11

--- src/main/java/com/skillshare/model/Comment.java
+++ src/main/java/com/skillshare/model/Comment.java
@@ -2,8 +2,15 @@ package com.skillshare.model;
 
 import java.time.Instant;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import java.time.Instant;
 
 public class Comment {
     @Id
