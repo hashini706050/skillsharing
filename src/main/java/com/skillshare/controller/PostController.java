109222d149f557bf792fd696adc9f2ead33fc9f8
PraveenUdara <praveen17vck@gmail.com>
2025-04-27 20:00:47 +0530

    commit-4-post-Controller-fixe-4

--- src/main/java/com/skillshare/controller/PostController.java
+++ src/main/java/com/skillshare/controller/PostController.java
@@ -1,3 +1,9 @@
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.skillshare.model.Post;
 
 public class PostController {
     private final PostRepository postRepository;
@@ -5,6 +11,16 @@ public class PostController {
 
     @GetMapping
     public ResponseEntity<List<Post>> getAllPosts() {
+         try {
+            log.debug("Fetching all posts");
+            List<Post> posts = postRepository.findAll();
+            return ResponseEntity.ok(posts);
+        } catch (Exception e) {
+            log.error("Error fetching posts", e);
+            throw new RuntimeException("Failed to fetch posts", e);
+        }
+    }
+
 
 
 
