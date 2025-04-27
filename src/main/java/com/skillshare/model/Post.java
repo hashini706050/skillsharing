f5950014a0b184227984238d4ddb8b74bf8fb9f8
PraveenUdara <praveen17vck@gmail.com>
2025-04-27 19:37:14 +0530

    commit-3-post-model-fixe-3

--- src/main/java/com/skillshare/model/Post.java
+++ src/main/java/com/skillshare/model/Post.java
@@ -1,11 +1,21 @@
 package com.skillshare.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "posts")
 
 public class Post {
    
