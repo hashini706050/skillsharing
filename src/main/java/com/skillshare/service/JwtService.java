2051a2eb809694ae07e185c98d5d36831ef9ddc9
Tharaka-Randeniya <dananjanirandeniya1@gmail.com>
2025-04-28 01:04:05 +0530

    commit-29-JwtService-service-fix-5

--- src/main/java/com/skillshare/service/JwtService.java
+++ src/main/java/com/skillshare/service/JwtService.java
@@ -1,3 +1,21 @@
+package com.skillshare.service;
+
+import com.skillshare.config.JwtConfig;
+import com.skillshare.model.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+
 public class JwtService {
     private final JwtConfig jwtConfig;
 
