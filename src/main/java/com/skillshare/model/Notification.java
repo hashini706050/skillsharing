37c67b25b146b5ec2588dc8df1d0e40feb91b4b7
Tharaka-Randeniya <dananjanirandeniya1@gmail.com>
2025-04-28 09:47:45 +0530

    commit-31-notification-model-fix-2

--- src/main/java/com/skillshare/model/Notification.java
+++ src/main/java/com/skillshare/model/Notification.java
@@ -1,3 +1,20 @@
+package com.skillshare.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "notifications")
+
 public class Notification {
     @Id
     private String id;
