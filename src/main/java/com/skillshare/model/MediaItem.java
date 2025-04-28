876f11db4b865500e897eded5805cb0c6040395e
PraveenUdara <praveen17vck@gmail.com>
2025-04-28 08:41:07 +0530

    commit-17-Media-Model-fixe-17

--- src/main/java/com/skillshare/model/MediaItem.java
+++ src/main/java/com/skillshare/model/MediaItem.java
@@ -1,10 +1,10 @@
+package com.skillshare.model;
 
-
-
-
-
-
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
 
 @Data
 @Builder
