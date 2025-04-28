bdb49e912f1093af0f6e8834d60e92d81645c625
PraveenUdara <praveen17vck@gmail.com>
2025-04-28 08:31:32 +0530

    commit-14-Media-MediaController-fixe-14

--- src/main/java/com/skillshare/controller/MediaController.java
+++ src/main/java/com/skillshare/controller/MediaController.java
@@ -1,11 +1,19 @@
+
 package com.skillshare.controller;
 
+import com.skillshare.model.MediaItem;
+import com.skillshare.service.MediaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequestMapping("/media")
+@RequiredArgsConstructor
 
 public class MediaController {
     private final MediaService mediaService;
